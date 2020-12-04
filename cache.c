c3bb090ae526c50aa506ed0d08366442a9cfef16
Franco-Mariotti <franco.mariotti@arbe.com.ar>
2020-12-04 14:29:23 -0300

    agrego archivo de definiciones cache.c

---
+++ cache.c
@@ -0,0 +1,37 @@
+#include "cache.h"
+
+void init() {
+
+}
+
+unsigned int find_set(int address) {
+
+}
+
+unsigned int find_lru(int setnum) {
+
+}
+
+unsigned int is_dirty(int way, int setnum) {
+
+}
+
+void read_block(int blocknum) {
+
+}
+
+void write_block(int way, int setnum) {
+
+}
+
+char read_byte(int address) {
+
+}
+
+void write_byte(int address, char value) {
+
+} 
+
+int get_miss_rate() {
+
+}
--- cache.h
+++ cache.h
@@ -2,10 +2,13 @@
 #define __CACHE_H_
 
 void init();
-unsigned int find_set(int address) unsigned int find_lru(int setnum);
-unsigned int is_dirty(int way, int setnum) void read_block(int blocknum);
+unsigned int find_set(int address);
+unsigned int find_lru(int setnum);
+unsigned int is_dirty(int way, int setnum);
+void read_block(int blocknum);
 void write_block(int way, int setnum);
 char read_byte(int address);
-void write_byte(int address, char value) int get_miss_rate();
+void write_byte(int address, char value);
+int get_miss_rate();
 
 #endif
