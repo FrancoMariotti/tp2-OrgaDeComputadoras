d04aa01287e0dd8541626dc5a093473cbd738b7a
Franco-Mariotti <franco.mariotti@arbe.com.ar>
2020-12-05 14:44:53 -0300

    agrego implementacion de cache_init

--- cache.c
+++ cache.c
@@ -1,59 +1,82 @@
 #include "cache.h"
+#include "stdlib.h"
 
-#define MEM_PRINCIPAL 4096
+#define MAIN_MEMORY 32768 //en cantidad de palabras
+#define WORD_SIZE 2 // cada palabra de 2 bytes
 
-uint16_t memPrincipal[MEM_PRINCIPAL]; // Memoria principal de 64KB
+#define VALID 1
+#define INVALID 0
 
-//El tamanio de palabra que utilizamos es de  2 bytes/16 bits
+int16_t memPrincipal[MAIN_MEMORY]; // Memoria principal de 64KB
 
 /* La via es un array de palabras y el numero de bloque 
  * dentro de la via queda determinado por el tamanio del bloque.
  */
 typedef struct bloque {
-    uint16_t tag;
-    uint16_t *palabras;
+  int tag;
+  int16_t *words;
+  char valid;
+  char dirty;
+  //para lru
+  char last_accessed;
 } bloque_t;
 
 struct cache {
-    int cVias;
-    int tamanio;
-    bloque_t *bloque;
+  int ways;
+  int size;
+  int miss_rate;
+  bloque_t *bloques;
 };
 
 /* Pre: La estructura cache fue inicializada
  */
-void cache_init(cache_t cache) {
+void cache_init(cache_t self) {
+  //incializo frecuencia de miss en cero.
+  self->miss_rate = 0;
+  //inicializa la memoria en cero.
+  memset(memPrincipal,0,WORD_SIZE*MAIN_MEMORY);
+
+  self->bloques = (bloque_t*)malloc(sizeof(bloque_t)*self->size);
+
+  for (size_t i = 0; i < size; i++) {
+    self->bloques[i]->dirty = 0;
+    self->bloques[i]->valid = INVALID;
+    self->bloques[i]->last_accessed = 0;
+  }
+}
+
+unsigned int cache_find_set(cache_t* self,int address) {
 
 }
 
-unsigned int cache_find_set(int address) {
+unsigned int cache_find_lru(cache_t* self,int setnum) {
 
 }
 
-unsigned int cache_find_lru(int setnum) {
+unsigned int cache_is_dirty(cache_t* self,int way, int setnum) {
 
 }
 
-unsigned int cache_is_dirty(int way, int setnum) {
+void cache_read_block(cache_t* self,int blocknum) {
 
 }
 
-void cache_read_block(int blocknum) {
+void cache_write_block(cache_t* self,int way, int setnum) {
 
 }
 
-void cache_write_block(int way, int setnum) {
+char cache_read_byte(cahce_t* self,int address) {
 
 }
 
-char cache_read_byte(int address) {
-
-}
-
-void cache_write_byte(int address, char value) {
+void cache_write_byte(cache_t* self,int address, char value) {
 
 } 
 
-int cache_get_miss_rate() {
+int cache_get_miss_rate(cache_t* self) {
+  return self->miss_rate;
+}
 
+void cache_destroy(cache_t* self) {
+  free(self->bloques);
 }
